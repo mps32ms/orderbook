bacf7636ac873df2fca710339a96a0b931936d19
mps32ms <mps32.ms@gmail.com>
2026-01-16 17:42:52 -0300

    chore: enforce component scan base package

--- src/main/java/com/marcospaulo/orderbook/OrderbookApplication.java
+++ src/main/java/com/marcospaulo/orderbook/OrderbookApplication.java
@@ -3,11 +3,11 @@ package com.marcospaulo.orderbook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.marcospaulo.orderbook")
 public class OrderbookApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrderbookApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrderbookApplication.class, args);
+    }
 
 }
